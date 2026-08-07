0f5037abf4a98890c80424b3f71de6cde55bd14c
Antriksh-s <antrikshsingh257@gmail.com>
2026-08-07 08:31:36 +0530

    Added remote state management using gcp bucket

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 1.5.0"
+
+  backend "gcs" {
+    bucket = "gcp-rundeck-tfstate" 
+    prefix = "rundeck/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
